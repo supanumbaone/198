1543e00641a5f2a2ae0a29c151dd85cfdd5e6965
Brent Bahry <brentbahry@gmail.com>
2011-02-24 14:55:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grooopme::Application.config.secret_token = '15e605781ca063fca37c6ebd0adffbacbe443909388adf6add689ec3e5c39bd1ff260745b380759f811aa5fc9a182504b76de1d2332e5084d266b52edb343eaf'
