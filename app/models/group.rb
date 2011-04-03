57737eec097be4c5c358d6bdc608fb71ed592c3c
Brent Bahry <brentbahry@gmail.com>
2011-04-02 17:14:28 -0700

    part way through the groups export

--- app/models/group.rb
+++ app/models/group.rb
@@ -18,4 +18,35 @@ class Group < ActiveRecord::Base
   validates_length_of :name, :within => 2..75, :too_long => "must be at most 75 characters", :too_short => "must be at least 2 characters"
   # validates_length_of :description, :within => 2..1500, :too_long => "must be at most 1500 characters", :too_short => "must be at least 2 characters"
   # validates_size_of :profile_image, :maximum => 5.megabytes
+  
+  def self.export
+    groups = Group.all
+    days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+    export_users = []
+    export_users << "Name,Email,,M,,,T,,,W,,,Th,,,F,,,S,,,Su,,Preferred Friends"
+    export_users << ",,am,noon,pm,am,noon,pm,am,noon,pm,am,noon,pm,am,noon,pm,am,noon,pm,am,noon,pm,"
+    groups.each do |group|
+      group.users.each do |user|
+        user_days = []
+        user_line = "#{user.first_name} #{user.last_name},#{user.email},"
+        if user.schedule
+          user.schedule.days.each do |day|
+            if day
+              day.time_blocks.each do |block|
+                if block
+                  user_days[days.index(day.name)] += "Y,"
+                else
+                  user_days[days.index(day.name)] += ","
+                end
+              end
+            end
+          end
+          
+          export_users.each do |e_user|
+            # here
+          end
+        end
+      end
+    end
+  end
 end
