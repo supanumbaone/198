f7630ee8b9878cdd4f255e2d76dc7e19c9dbf922
Brent Bahry <brentbahry@gmail.com>
2011-04-04 21:03:11 -0700

    added the download links to roster.html.erb for sake of forming the groups

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,20 +1,20 @@
-require 'tlsmail'    
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+# require 'tlsmail'    
+# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
 
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,  
-  :address            => 'smtp.gmail.com',
-  :port               => 587,
-  :tls                  => true,
-  :domain             => 'gmail.com', #you can also use google.com
-  :authentication     => :plain,
-  :user_name          => 'grooopme@gmail.com',
-  :password           => 'yap2ci2wad6in4j'
-}
+# ActionMailer::Base.smtp_settings = {
+  # :enable_starttls_auto => true,  
+  # :address            => 'smtp.gmail.com',
+  # :port               => 587,
+  # :tls                  => true,
+  # :domain             => 'gmail.com', #you can also use google.com
+  # :authentication     => :plain,
+  # :user_name          => 'grooopme@gmail.com',
+  # :password           => 'yap2ci2wad6in4j'
+# }
 
 =begin
 ActionMailer::Base.smtp_settings = {
